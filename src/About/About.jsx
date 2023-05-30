f0529caeaaabb9abe1e39f68bf6abab3b3c7c0d8
mdasif526976 <mdasif526976@gmail.com>
2023-05-30 20:36:42 +0600

    About page create

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -2,32 +2,44 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <div className="w-full justify-center p-8 sm:flex sm:space-x-6 bg-gray-50 text-black">
-	<div className="flex-shrink-0 w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
-		<img src="https://source.unsplash.com/100x100/?portrait?1" alt="" className="object-cover object-center w-full h-full rounded bg-gray-500" />
-	</div>
-	<div className="flex flex-col space-y-4">
-		<div>
-			<h2 className="text-2xl font-semibold">Leroy Jenkins</h2>
-			<span className="text-sm text-gray-400">General manager</span>
-		</div>
-		<div className="space-y-1">
-			<span className="flex items-center space-x-2">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Email address" className="w-4 h-4">
-					<path fill="currentColor" d="M274.6,25.623a32.006,32.006,0,0,0-37.2,0L16,183.766V496H496V183.766ZM464,402.693,339.97,322.96,464,226.492ZM256,51.662,454.429,193.4,311.434,304.615,256,268.979l-55.434,35.636L57.571,193.4ZM48,226.492,172.03,322.96,48,402.693ZM464,464H48V440.735L256,307.021,464,440.735Z"></path>
-				</svg>
-				<span className="text-gray-400">leroy.jenkins@company.com</span>
-			</span>
-			<span className="flex items-center space-x-2">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Phonenumber" className="w-4 h-4">
-					<path fill="currentColor" d="M449.366,89.648l-.685-.428L362.088,46.559,268.625,171.176l43,57.337a88.529,88.529,0,0,1-83.115,83.114l-57.336-43L46.558,362.088l42.306,85.869.356.725.429.684a25.085,25.085,0,0,0,21.393,11.857h22.344A327.836,327.836,0,0,0,461.222,133.386V111.041A25.084,25.084,0,0,0,449.366,89.648Zm-20.144,43.738c0,163.125-132.712,295.837-295.836,295.837h-18.08L87,371.76l84.18-63.135,46.867,35.149h5.333a120.535,120.535,0,0,0,120.4-120.4v-5.333l-35.149-46.866L371.759,87l57.463,28.311Z"></path>
-				</svg>
-				<span className="text-gray-400">+25 381 77 983</span>
-			</span>
-		</div>
-	</div>
-</div>
+        <div className='mx-auto my-5 bg-gray-50 rounded w-10/12 '>
+                        <h1 className='text-center text-2xl fonst-bold py-3 font-sans 
+                        mx-2'>About Me</h1>
+            <div className='flex md:flex-row flex-col gap-5  justify-around'>
+           <div>
+            <img className='w-80 h-full rounded' src="https://i.ibb.co/XYpkSvX/DSC-0075.jpg" alt="" />
+           </div>
+           <div className='grid  grid-cols-1 md:grid-cols-2 mx-auto md:mx-3 gap-5'>
+             <div className='w-72 rounded h-52 bg-gray-200'>
+                <h4 className='text-center font-bold text-xl'>Introduce</h4>
+                <div className='m-2 text-sm font-semibold'>
+                I was born in BanglaDesh. My home town is Brahmanbaria. Now I live in Khayaser in Brahmanbaria.
+                </div>
+             </div>
+             <div className='w-72 rounded h-52 bg-gray-200'>
+                <h4 className='text-center font-bold text-xl'>Education</h4>
+                <div className='m-2 text-sm font-semibold'>
+                   Hello i'm Asif.I complate my 10th class.Now i'm reading inter 1st Year.
+                </div>
+             </div>
+             <div className='w-72 rounded h-52 bg-gray-200'>
+                <h4 className='text-center font-bold text-xl'>Contract</h4>
+                <div className='m-2 text-sm font-semibold'>
+               Email:mdasif526976@gmail.com
+               Linkedin:https://www.linkedin.com/in/md-asif-b841bb247/
+               github:https://github.com/mdasif526976
+                </div>
+             </div>
+             <div className='w-72 rounded h-52 bg-gray-200'>
+                <h4 className='text-center font-bold text-xl'>Work Technology</h4>
+                <div className='m-2 text-sm font-semibold'>
+           Im a Mern Stack Developer. I have worked in both Front-End & Back-End. I have worked in technology MongoDB, Express, React js & Node js. I have good knowledge of Html, Css
+& Javascript. For Frontend, I have worked with both Bootstrap & Tailwind Css.
+                </div>
+             </div>
+             
+           </div>
+ </div>
         </div>
     );
 };
